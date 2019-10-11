e174a449ad3b4064527a2ccc2e74002696d033f2
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-10-10 19:56:33 -0700

    Update TestCircleWithPrivateDataField.java

--- TestCircleWithPrivateDataField.java
+++ TestCircleWithPrivateDataField.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author szkola
- */
 public class TestCircleWithPrivateDataField {
-    /** Main method */ 
+    
+    /** Main method of course it is! */ 
     public static void main(String[] args) {
         
 // Create a Circle with radius 5.0
