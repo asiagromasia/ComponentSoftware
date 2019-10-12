d145fb9b87afe5145b376839622d540097a1fada
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-10-11 22:52:14 -0700

    Update TestCircleWithPrivateDataField.java

--- TestCircleWithPrivateDataField.java
+++ TestCircleWithPrivateDataField.java
@@ -1,13 +1,13 @@
 public class TestCircleWithPrivateDataField {
     
-    /** Main method of course it is! */ 
     public static void main(String[] args) {
         
-// Create a Circle with radius 5.0
-    Circle myCircle = new Circle (5.0);    
-    System.out.println("The area of the circle of radius "      + myCircle.getRadius() + " is " + myCircle.getArea());
-// Increase myCircle's radius by 10%
-    myCircle.setRadius(myCircle.getRadius() * 1.1); 
-    System.out.println("The area of the circle of radius "      + myCircle.getRadius() + " is " + myCircle.getArea()); 
-}
+        // Create a Circle with radius 5.0
+        Circle myCircle = new Circle (5.0);    
+        System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea());
+
+        // Increase myCircle's radius by 10%
+        myCircle.setRadius(myCircle.getRadius() * 1.1); 
+        System.out.println("The area of the circle of radius " + myCircle.getRadius() + " is " + myCircle.getArea()); 
+    }
 }
